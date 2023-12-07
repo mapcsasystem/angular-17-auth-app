2cdfb4c14cc26ba30ab9a42a7af0f5ab61b80cd5
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-12-07 16:01:06 -0600

    create AuthService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { environment } from '../../../environments/environment.development';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl: string = environment.baseUrl;
+  private readonly http = inject(HttpClient);
 
-  constructor() { }
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  constructor() {}
+
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
