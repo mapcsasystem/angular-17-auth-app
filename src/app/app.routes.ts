1377aab3c0d2d55b183ee7de7d100fb6a36de84d
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-12-07 15:17:53 -0600

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./auth/auth.component').then((c) => c.AuthComponent),
+    children: [
+      {
+        path: 'login',
+        title: 'Login',
+        loadComponent: () =>
+          import('./auth/pages/login-page/login-page.component').then(
+            (c) => c.LoginPageComponent
+          ),
+      },
+      {
+        path: 'register',
+        title: 'Registrar',
+        loadComponent: () =>
+          import('./auth/pages/register-page/register-page.component').then(
+            (c) => c.RegisterPageComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'login',
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    loadComponent: () =>
+      import('./dasboard/dasboard.component').then((c) => c.DasboardComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
+];
