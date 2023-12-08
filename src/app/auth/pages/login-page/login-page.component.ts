e127c2fdd6cd935eb96ca8a119134752765d2489
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-12-07 19:33:58 -0600

    show Alert

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,7 +1,17 @@
-import { Component, inject } from '@angular/core';
+//#region Imports
+import { Component, OnDestroy, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../../services/auth.service';
+import { LoginRequest } from '../../interfaces/login-request';
+
+import Swal from 'sweetalert2';
+//#endregion
+
+//#region @Component
 @Component({
   selector: 'app-login-page',
   standalone: true,
@@ -9,14 +19,47 @@ import { RouterModule } from '@angular/router';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {
+//#endregion
+export class LoginPageComponent implements OnDestroy {
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   public myForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  summit() {
-    console.log(this.myForm.value);
+  private _sub = new Subscription();
+
+  summit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { email, password } = this.myForm.value as LoginRequest;
+    this._sub.add(
+      this.authService.login(email, password).subscribe({
+        next: (value) => {
+          console.log(value);
+          this.router.navigateByUrl('/dashboard', { replaceUrl: true });
+        },
+        error: (errorMessage) => {
+          if (errorMessage === 401) {
+            Swal.fire({
+              title: 'Error',
+              text: 'Las correo ó contraseña incorrectos',
+              icon: 'error',
+              confirmButtonText: 'Aceptar',
+            });
+          }
+        },
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._sub.unsubscribe();
   }
 }
